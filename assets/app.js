585cd79486a12152835ce6646b98e46f509e9eee
dieguito711 <diegosalischiker711@gmail.com>
2019-10-03 11:35:23 -0300

    Cambios Css Select y agregar id a los tiles generados

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,13 @@
 //Function that creates the board according to the size the user selected
 function drawHTMLBoard(selectedSize) {
-    console.log("Entré a la función");
     var container = $(".container");
     container.empty();
-    quantCards = selectedSize.value * selectedSize.value;
-    console.log("quantCards = " + quantCards);
+    quantTiles = selectedSize.value * selectedSize.value;
     container.css({
         "grid-template-columns": "repeat(" + selectedSize.value + ", 1fr)",
         "grid-template-rows": "repeat(" + selectedSize.value + ",1fr)"
     });
-    for (let i = 0; i < quantCards; i++) {
-        console.log("Entré al for");
-        container.append("<div>" + i + "</div>");
+    for (let i = 0; i < quantTiles; i++) {
+        container.append("<div id='tile" + i + "'>" + i + "</div>");
     }
 }
