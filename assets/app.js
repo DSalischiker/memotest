af9f69833ef9153d3acbd89307b228d65b640330
dieguito711 <diegosalischiker711@gmail.com>
2019-10-03 12:40:50 -0300

    JAVASCRIPT random para agregar valores a tablero lógico

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,53 @@
+var board = [];
 //Function that creates the board according to the size the user selected
-function drawHTMLBoard(selectedSize) {
+function drawBoard(selectedSize) {
+    var boardSize = selectedSize.value;
+    var totalTiles = boardSize * boardSize;
+    createLogicalBoard(boardSize);
+    setTilesLogicalBoard(boardSize, totalTiles);
     var container = $(".container");
     container.empty();
-    quantTiles = selectedSize.value * selectedSize.value;
+
     container.css({
-        "grid-template-columns": "repeat(" + selectedSize.value + ", 1fr)",
-        "grid-template-rows": "repeat(" + selectedSize.value + ",1fr)"
+        "grid-template-columns": "repeat(" + boardSize + ", 1fr)",
+        "grid-template-rows": "repeat(" + boardSize + ",1fr)"
     });
-    for (let i = 0; i < quantTiles; i++) {
+    for (let i = 0; i < totalTiles; i++) {
         container.append("<div id='tile" + i + "'>" + i + "</div>");
+    }
+    /* console.log(randomposition(quantTiles)); */
+}
+
+function createLogicalBoard(boardSize) {
+
+    for (var i = 0; i < boardSize; i++) {
+        board[i] = [];
+        for (var j = 0; j < boardSize; j++) {
+            board[i][j] = "";
+        }
+    }
+    console.log(board);
+}
+
+function setTilesLogicalBoard(boardSize, totalTiles) {
+    position = randomposition(boardSize);
+    for (let i = 0; i < totalTiles; i++) {
+
+        if (board[position[0]][position[1]] === "") {
+            board[position[0]][position[1]] = loadTile();
+            console.log("Posición " + position + " = " + board[position[0]][position[1]]);
+        }
     }
 }
 
-var tablero = [];
+function randomposition(boardSize) {
+    /* return Math.floor(Math.random() * (totalTiles)); */
+    var position = Math.floor(Math.random() * (boardSize));
+    position = position + "" + Math.floor(Math.random() * (boardSize));
+    console.log(position);
+    return position;
+}
 
-for( i=0; i < selectedSize.value; i++){
-    tablero[i] = new Array(selectedSize.value);
-};
+function loadTile() {
+    return Math.floor(Math.random() * (18 - 1)) + 1;
+}
